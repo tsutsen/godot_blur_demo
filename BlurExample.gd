1860397afa195a6d4ab4077d0a68b3a481de6170
tsutsen <harlov.leonid@ya.ru>
2025-01-18 00:56:42 +0500

    update gaussian blur and add lod blur

--- BlurExample.gd
+++ BlurExample.gd
@@ -2,9 +2,19 @@ extends Control
 
 func _ready():
 	$%BlurRadiusSlider.value_changed.connect(_blur_changed)
+	$%BlurLodSlider.value_changed.connect(_lod_changed)
+
 	_blur_changed($%BlurRadiusSlider.value)
+	_lod_changed($%BlurLodSlider.value)
+
 
 func _blur_changed(radius: float):
+	print('blur')
 	$%BlurX.material.set_shader_parameter("radius", radius)
 	$%BlurY.material.set_shader_parameter("radius", radius)
 	$%BlurRadiusLabel.text = "%.2f" % [radius]
+
+func _lod_changed(lod: float):
+	print('lod')
+	$%BlurLod.material.set_shader_parameter("intensity", lod)
+	$%LodLabel.text = "%.2f" % [lod]
